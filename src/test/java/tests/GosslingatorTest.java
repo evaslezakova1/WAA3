afa35774e98c26a97ea3a0038fa035d4b2d2834b
eva.slezakova <eva.slezakova1@gmail.com>
2019-05-06 19:02:28 +0200

    vynatie casti before a after do samostatnych

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -68,62 +68,48 @@ public class GosslingatorTest {
     @Test
     public void itShouldAddFiftyRyans() {
 //      ALT + J oznacenie kazdeho dalsieho vyskytu
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-        addRyanButton.click();
-
+        // for cyclus: zadefinujem si premennu, ktorej urcim hodnotu 0: (int i=0;
+        // urcim, kolkokrat ma opakovat cyklus (aj prve cislo je iteracia): i<50;
+        // zvysim i o 1: i++)
+        // do tela vlozim prikaz, ktory sa ma cyklit: {addRyanButton.click();}
+        // skratka je fori + tab
+        for (int i = 0; i < 50; i++) {
+            addRyanButton.click();
+            String actualNrRyans = driver.findElement(By.id("ryanCounter")).getText();
+            //musim int zmenit na string
+            Assert.assertEquals(String.valueOf(i + 1), actualNrRyans);
+            String actualLabelOfRyan = driver.findElement(By.cssSelector("div.ryan-counter h3")).getText();
+            //if - do zatvorky dam podmienku a do {} vlozim akciu
+            // ak chce pokracovat s else, tak hned else a do {} vlozim akciu
+            //alternativa podmienky (i + 1 == 1)
+            if (i < 1) {
+                Assert.assertEquals("ryan", actualLabelOfRyan);
+                //alternativa podmienky (i + 1 >= 2) -> samostatne if nie vetvene
+            } else {
+                Assert.assertEquals("ryans", actualLabelOfRyan);
+            }
+        }
         String alertMessage = driver.findElement(By.cssSelector("h1.tooManyRyans")).getText();
         System.out.println(alertMessage);
         Assert.assertEquals("NUMBER OF\n" +
                 "RYANS\n" +
                 "IS TOO DAMN\n" +
                 "HIGH", alertMessage);
+    }
+    @Test
+    public void usingWhileCycle(){
+        String actualNrRyans = driver.findElement(By.id("ryanCounter")).getText();
+        int clicksLimit = 30;
+        int clicks = 0;
+        // while cyclus - vykona sa vzdy, ak je podmienka true
+        // pre while sa rozhodneme, ak nevieme jednoznacne povedat, do kedy by for bezal
+        // zaroven = &&
+        // alebo = ||
+        while (!actualNrRyans.equals("50")&& clicks < clicksLimit){
+            addRyanButton.click();
+            actualNrRyans = driver.findElement(By.id("ryanCounter")).getText();
+            clicks++;
+        }
+
     }
 }
